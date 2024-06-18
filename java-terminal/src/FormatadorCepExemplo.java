5fe6d87f555fc1d873a12aed421ebdbd914bd2a9
Tharlis Sampaio <tharlis.sampaio@outlook.com>
2024-06-17 21:41:14 -0500

    fix - correção de código

--- java-terminal/src/FormatadorCepExemplo.java
+++ java-terminal/src/FormatadorCepExemplo.java
@@ -2,6 +2,7 @@ public class FormatadorCepExemplo {
     public static void main(String[] args) {
         try {
             String cepFormatado = formatarCep("2376504");
+            System.out.println(cepFormatado);
         } catch (CepInvalidoException e) {
             // TODO Auto-generated catch block
             System.out.println("o cep não corresponde com a regra de negocio");
